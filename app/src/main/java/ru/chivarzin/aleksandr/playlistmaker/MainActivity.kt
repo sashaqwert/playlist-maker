4e8a7b890bff3693a3a87ff721777002077cf2a4
Aleksandr Chivarzin <aleksandr@chivarzin.ru>
2026-02-25 15:07:55 +0300

    Обработка нажатия кнопок на главном экране Toast

--- app/src/main/java/ru/chivarzin/aleksandr/playlistmaker/MainActivity.kt
+++ app/src/main/java/ru/chivarzin/aleksandr/playlistmaker/MainActivity.kt
@@ -1,6 +1,9 @@
 package ru.chivarzin.aleksandr.playlistmaker
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,5 +14,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val button_search = findViewById<Button>(R.id.button_search)
+        val button_mediateka = findViewById<Button>(R.id.button_mediateka)
+        val button_settings = findViewById<Button>(R.id.button_settings)
+
+        val searchClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                Toast.makeText(this@MainActivity, "Нажата кнопка поиска", Toast.LENGTH_SHORT).show()
+            }
+        }
+        button_search.setOnClickListener(searchClickListener)
+
+        button_mediateka.setOnClickListener { view ->
+            Toast.makeText(this@MainActivity, "Нажата кнопка \"Медиатека\"", Toast.LENGTH_SHORT).show()
+        }
+        button_settings.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажата кнопка \"Настройки\"", Toast.LENGTH_SHORT).show()
+        }
     }
 }
