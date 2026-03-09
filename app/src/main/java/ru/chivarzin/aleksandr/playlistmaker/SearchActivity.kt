0b447df378bb3442bd524b43f9bccb731892cd32
Aleksandr Chivarzin <aleksandr@chivarzin.ru>
2026-03-09 17:44:12 +0300

    Разметка и код поиска для светлой темы

--- app/src/main/java/ru/chivarzin/aleksandr/playlistmaker/SearchActivity.kt
+++ app/src/main/java/ru/chivarzin/aleksandr/playlistmaker/SearchActivity.kt
@@ -1,7 +1,14 @@
 package ru.chivarzin.aleksandr.playlistmaker
 
+import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -22,5 +29,31 @@ class SearchActivity : AppCompatActivity() {
         action_back.setOnClickListener {
             finish()
         }
+
+        val search = findViewById<EditText>(R.id.search)
+        val clear_search = findViewById<TextView>(R.id.clear_search)
+        clear_search.setOnClickListener {
+            search.setText("")
+            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+            inputMethodManager?.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
+        }
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // empty
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s.isNullOrEmpty()) {
+                    clear_search.visibility = View.GONE
+                } else {
+                    clear_search.visibility = View.VISIBLE
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // empty
+            }
+        }
+        search.addTextChangedListener(simpleTextWatcher)
     }
 }
