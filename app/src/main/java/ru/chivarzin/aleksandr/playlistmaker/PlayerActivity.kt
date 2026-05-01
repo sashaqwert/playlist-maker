3ab88c26679e53d9c303d31b6f0f4e4ae9f80e10
Aleksandr Chivarzin <aleksandr@chivarzin.ru>
2026-05-01 17:35:30 +0300

    Кнопка "назад" в плеере

--- app/src/main/java/ru/chivarzin/aleksandr/playlistmaker/PlayerActivity.kt
+++ app/src/main/java/ru/chivarzin/aleksandr/playlistmaker/PlayerActivity.kt
@@ -1,6 +1,7 @@
 package ru.chivarzin.aleksandr.playlistmaker
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -21,5 +22,9 @@ class PlayerActivity : AppCompatActivity() {
             insets
         }
         track = Gson().fromJson<Track>(intent.getStringExtra("track"), Track::class.java)
+        val player_action_back = findViewById<ImageView>(R.id.player_action_back)
+        player_action_back.setOnClickListener {
+            finish()
+        }
     }
 }
