153b30ea6e6be5bfdbc2af8144c86497711709a0
Aleksandr Chivarzin <aleksandr@chivarzin.ru>
2026-06-09 13:55:47 +0300

    Использование TrackDto

--- app/src/main/java/ru/chivarzin/aleksandr/playlistmaker/data/dto/SearchResult.kt
+++ app/src/main/java/ru/chivarzin/aleksandr/playlistmaker/data/dto/SearchResult.kt
@@ -3,4 +3,4 @@ package ru.chivarzin.aleksandr.playlistmaker.data.dto
 import ru.chivarzin.aleksandr.playlistmaker.domain.models.Track
 
 data class SearchResult (val resultCount: Int,
-                         val results: List<Track>) : Response()
+                         val results: List<TrackDto>) : Response()
