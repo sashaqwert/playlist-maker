c9d559b0fe6b9dc73e183f8953143af3b6cbcf41
Aleksandr Chivarzin <aleksandr@chivarzin.ru>
2026-07-21 18:32:06 +0300

    Медиатека: Title и action_back

--- app/src/main/java/ru/chivarzin/aleksandr/playlistmaker/ui/mediateka/MediatekaActivity.kt
+++ app/src/main/java/ru/chivarzin/aleksandr/playlistmaker/ui/mediateka/MediatekaActivity.kt
@@ -1,6 +1,7 @@
 package ru.chivarzin.aleksandr.playlistmaker.ui.mediateka
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,6 +17,10 @@ class MediatekaActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val action_back = findViewById<ImageView>(R.id.mediateka_action_back)
+        action_back.setOnClickListener {
+            finish()
         }
     }
 }
