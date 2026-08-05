3a1d662dfa59b3fd0efb83571bc4a946898d03a3
Aleksandr Chivarzin <aleksandr@chivarzin.ru>
2026-08-05 18:43:25 +0300

    Нижняя панель навигации в MainActivity

--- app/src/main/java/ru/chivarzin/aleksandr/playlistmaker/ui/main/MainActivity.kt
+++ app/src/main/java/ru/chivarzin/aleksandr/playlistmaker/ui/main/MainActivity.kt
@@ -4,7 +4,13 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import ru.chivarzin.aleksandr.playlistmaker.R
 import ru.chivarzin.aleksandr.playlistmaker.ui.mediateka.MediatekaActivity
 import ru.chivarzin.aleksandr.playlistmaker.ui.search.SearchActivity
@@ -13,28 +19,18 @@ import ru.chivarzin.aleksandr.playlistmaker.ui.settings.SettingsActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //enableEdgeToEdge()
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-
-        val button_search = findViewById<Button>(R.id.button_search)
-        val button_mediateka = findViewById<Button>(R.id.button_mediateka)
-        val button_settings = findViewById<Button>(R.id.button_settings)
-
-        val searchClickListener: View.OnClickListener = object : View.OnClickListener {
-            override fun onClick(p0: View?) {
-                val intent = Intent(this@MainActivity, SearchActivity::class.java)
-                startActivity(intent)
-            }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
         }
-        button_search.setOnClickListener(searchClickListener)
 
-        button_mediateka.setOnClickListener { view ->
-            val intent = Intent(this@MainActivity, MediatekaActivity::class.java)
-            startActivity(intent)
-        }
-        button_settings.setOnClickListener {
-            val intent = Intent(this@MainActivity, SettingsActivity::class.java)
-            startActivity(intent)
-        }
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.container_view) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
