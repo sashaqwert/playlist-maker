e508df7744e22f60d5c7d4abb00d9c1c6564bc84
Aleksandr Chivarzin <aleksandr@chivarzin.ru>
2026-08-25 17:41:10 +0300

    Библиотека корутин

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ android {
 }
 
 dependencies {
+    implementation(libs.kotlinx.coroutines.android)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.androidx.fragment.ktx)
