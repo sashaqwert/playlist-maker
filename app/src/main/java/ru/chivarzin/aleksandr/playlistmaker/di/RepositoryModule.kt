550992480f6ea0c5eb8f4555fa8487c382166ca8
Aleksandr Chivarzin <aleksandr@chivarzin.ru>
2026-09-07 16:55:12 +0300

    Подключен FavoriteRepository к di

--- app/src/main/java/ru/chivarzin/aleksandr/playlistmaker/di/RepositoryModule.kt
+++ app/src/main/java/ru/chivarzin/aleksandr/playlistmaker/di/RepositoryModule.kt
@@ -3,12 +3,14 @@ package ru.chivarzin.aleksandr.playlistmaker.di
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 import ru.chivarzin.aleksandr.playlistmaker.data.converters.TrackDbConverter
+import ru.chivarzin.aleksandr.playlistmaker.data.db.FavoriteRepositoryImpl
 import ru.chivarzin.aleksandr.playlistmaker.data.network.TracksRepositoryImpl
 import ru.chivarzin.aleksandr.playlistmaker.data.storage.SearchHistoryRepositoryImpl
 import ru.chivarzin.aleksandr.playlistmaker.data.storage.ThemeRepositoryImpl
 import ru.chivarzin.aleksandr.playlistmaker.domain.api.SearchHistoryRepository
 import ru.chivarzin.aleksandr.playlistmaker.domain.api.ThemeRepository
 import ru.chivarzin.aleksandr.playlistmaker.domain.api.TracksRepository
+import ru.chivarzin.aleksandr.playlistmaker.domain.db.FavoriteRepository
 
 val repositoryModule = module {
     single<TracksRepository> {
@@ -25,5 +27,9 @@ val repositoryModule = module {
 
     single {
         TrackDbConverter()
+    }
+
+    single<FavoriteRepository> {
+        FavoriteRepositoryImpl(get(), get())
     }
 }
